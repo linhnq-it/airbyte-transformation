cd9230a2eae240ba940176f26ee98ea2c51146a5
linhnq <linhnq@meeyland.com>
2023-03-02 14:00:13 +0700

    update

--- models/tranform/tf_logs.sql
+++ models/tranform/tf_logs.sql
@@ -1,7 +1,7 @@
 select
     a.clientid Client_id,
     b.name client_name
-from {{ ref('meey_log_logs') }} a
-left join {{ ref('meey_log_clients') }} b
+from {{ ref('logs') }} a
+left join {{ ref('clients') }} b
     on a.clientid = b.id
 order by id desc
